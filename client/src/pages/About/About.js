340af62725297356ef67f4c311f6aadae5f6ae39
web-collab <tanmoy587@gmail.com>
2024-06-04 21:29:08 +0530

    About Section

---
+++ client/src/pages/About/About.js
@@ -0,0 +1,35 @@
+import React from "react";
+import "./About.css";
+const About = () => {
+  return (
+    <>
+      <div className="about">
+        <div className="row">
+          <div className="col-md-6 col-xl-6 col-lg-6 col-xs-12 about-img">
+            <img
+              src="https://th.bing.com/th/id/OIP.l7wFjokaGcB5GQ8TkV-hlwHaHK?w=221&h=213&c=7&r=0&o=5&dpr=1.1&pid=1.7"
+              alt="profile_pic"
+            />
+          </div>
+          <div className="col-md-6 col-xl-6 col-lg-6 col-xs-12 about-content">
+            <h1>About me</h1>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut
+              similique sequi velit aperiam totam quis iusto? Deleniti,
+              similique id, sunt porro eos neque ab sit nihil culpa labore
+              perferendis autem rem nostrum asperiores laudantium corrupti atque
+              voluptatibus voluptatum officiis. Soluta alias autem, commodi quos
+              suscipit modi maiores possimus! Placeat maiores assumenda
+              inventore vel doloribus, sunt non. Sequi, corporis harum dolorem
+              numquam, quis nesciunt atque tempore quae? Quibusdam consequatur
+              quidem pariatur hic, ipsam eum beatae labore tempora quis fugit?
+              Vitae, aperiam velit.
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default About;
