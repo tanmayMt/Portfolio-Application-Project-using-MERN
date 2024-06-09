54483acae75ffc08b369772d0ee2b3ac9767bc21
web-collab <tanmoy587@gmail.com>
2024-06-09 11:46:07 +0530

    1.Creating Node Server APIs

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const cors = require('cors')
+const dotenv = require('dotenv')
+
+//dotenv configuartion
+dotenv.config();
+
+//rest object
+const app = express();
+
+//midlewares
+app.use(cors());
+app.use(express.json());
+
+//routes
+//app.use("/api/v1/portfolio", require("./routes/portfolioRoute"));
+app.get('/',(req,res)=>{
+  res.send('<h1>Welcome To Node Server</h1>')
+})
+
+//port
+const PORT = process.env.PORT || 8080;
+
+//listen
+app.listen(PORT, () => {
+  console.log(`Server Runnning On PORT ${PORT} `);
+});
